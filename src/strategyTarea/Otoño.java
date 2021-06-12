ae194640de3b8bcf333379e578e400564e7c5130
ignacio <ignacio.valencia.sauma@gmail.com>
2021-06-12 19:56:14 -0400

    tareaStrategy

--- src/strategyTarea/Otoño.java
+++ src/strategyTarea/Otoño.java
@@ -5,7 +5,7 @@ public class Otoño implements IStrategy {
 	@Override
 	public void execute(double precio) {
 		double descuento = precio * 0.5;
-		System.out.println("Precio invierno: " + (precio - descuento));
+		System.out.println("Precio otoño: " + (precio - descuento));
 	}
 
 }
